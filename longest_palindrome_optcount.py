a5845047d516ca5efd76867ffb080522a8768763
wychu413 <78985672+wychu413@users.noreply.github.com>
2021-05-26 11:22:01 +0800

    Update longest_palindrome_optcount.py
    finish

--- longest_palindrome_optcount.py
+++ longest_palindrome_optcount.py
@@ -1,1 +1,26 @@
+from collections import Counter
 
+"""leetcode: 409. Longest Palindrome"""
+def longest_palindrome(s: str) -> int:
+    """find the length of longest palindrome of string s.
+    
+    Parameters
+    ----------
+    s: string/characters used to construct the longest palindromes
+    """
+    ret = 0
+    carrier = 0
+    if len(s) == 0: return ret
+    for v in Counter(s).itervalues():
+        if v % 2 == 0:
+            ret += v
+        elif v % 2 == 1 and v > 1:
+            ret += v - 1
+            carrier = 1
+        elif v == 1:
+            carrier = 1
+    return ret + carrier
+
+assert longest_palindrome("abccccdd") == 7
+assert longest_palindrome("a") == 1
+assert longest_palindrome("bb") == 2
